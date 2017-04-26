099b61bd0eca2bbdf6bdc72dd2dfb11c88d34d9a
Ritvars Timermanis <me@richusx.me>
2017-04-26 20:23:35 +0100

    Added nameday search feature

--- namedaycog.py
+++ namedaycog.py
@@ -2,19 +2,34 @@ import discord
 import json
 import datetime
 import os
+import re
 from discord.ext import commands
+
+def findName(w):
+    return re.compile(r'\b({0})\b'.format(w), flags=re.IGNORECASE).search
 
 class Namedaycog:
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command()
-    async def vd(self):
+    @commands.command(pass_context=True)
+    async def vd(self, ctx, msg:str=None):
         ''' Reply with todays name-days '''
         path = os.path.dirname(os.path.realpath(__file__))
         t = datetime.datetime.now()
         vd = json.loads(open("%s/namedays.json" % (path)).read())
-        await self.bot.say("[VDiena] - [Šodien: '%s', Neiekļautie: '%s'] - %s" % (vd["namedays"][int(t.strftime('%j'))]["names"], vd["namedays"][int(t.strftime('%j'))]["noncalendarnames"], t.strftime('%d.%m.%Y')))
+        if msg is None:
+            await self.bot.say("[VDiena] - [Šodien: '%s', Neiekļautie: '%s'] - %s" % (vd["namedays"][int(t.strftime('%j'))]["names"], vd["namedays"][int(t.strftime('%j'))]["noncalendarnames"], t.strftime('%d.%m.%Y')))
+        else:
+            nameFound = False
+            for day in vd["namedays"]:
+                if findName(msg)(day["names"]):
+                    await self.bot.say("[VDiena] %s vārda dienu svin %s.%s." % (msg, day["day"], day["month"]))
+                    nameFound = True
+            if not nameFound:
+                await self.bot.say("[VDiena] - Vārds netika atrasts")
+
+
 
 def setup(bot):
     bot.add_cog(Namedaycog(bot))
