275e595be92e255c9f51d935e5f11327d1fa531e
Ritvars Timermanis <me@richusx.me>
2017-04-26 20:39:09 +0100

    Added help message

--- namedaycog.py
+++ namedaycog.py
@@ -20,6 +20,8 @@ class Namedaycog:
         vd = json.loads(open("%s/namedays.json" % (path)).read())
         if msg is None:
             await self.bot.say("[VDiena] - [Šodien: '%s', Neiekļautie: '%s'] - %s" % (vd["namedays"][int(t.strftime('%j'))]["names"], vd["namedays"][int(t.strftime('%j'))]["noncalendarnames"], t.strftime('%d.%m.%Y')))
+        elif msg.lower() == "help":
+             await self.bot.say("```\n!vd - Šodienas vārda dienu jubilāri\n!vd [vārds] - Datums kurā [vārds] svin vārda dienu```")
         else:
             nameFound = False
             for day in vd["namedays"]:
@@ -27,7 +29,7 @@ class Namedaycog:
                     await self.bot.say("[VDiena] %s vārda dienu svin %s.%s." % (msg, day["day"], day["month"]))
                     nameFound = True
             if not nameFound:
-                await self.bot.say("[VDiena] - Vārds netika atrasts")
+                await self.bot.say('[VDiena] - Vārds "%s" netika atrasts' % (msg))
 
 
 
