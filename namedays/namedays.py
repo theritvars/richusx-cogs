21a035c49ae556c6fe1b8a5416ceace80495cc1a
Ritvars Timermanis <me@richusx.me>
2018-03-01 12:55:58 +0000

    Re-wrote namedays.py

--- namedays/namedays.py
+++ namedays/namedays.py
@@ -9,7 +9,6 @@ import urllib.request
 emoji = ":champagne:"  # Iekļautie
 emoji2 = ":beers:"  # Neiekļautie
 
-
 def findname(w):
     return re.compile(r'\b({0})\b'.format(w), flags=re.IGNORECASE).search
 
@@ -20,45 +19,62 @@ class Namedays:
         self.data = json.loads(open("data/namedays/namedays.json", encoding='utf-8').read())
         self.bot = bot
 
+    def findByDate(self, day, month):
+        vd = self.data
+        for i in vd["namedays"]:
+            if i["month"] == month:
+                if i["day"] == day:
+                    return [i["names"], i["noncalendarnames"]]
+        return False
+
+    def findByName(self, name):
+        vd = self.data
+        for i in vd["namedays"]:
+            if name.lower() in i["names"].lower() or name.lower() in i["noncalendarnames"].lower():
+                date = "%s/%s" % (i["day"], i["month"])
+                return date
+        return False
+
+    def notFound(msg):
+        await self.bot.say("Kļūda! '%s' netika atrasts!" % (msg))
+       
+
     @commands.command(pass_context=True)
     async def vd(self, ctx, msg: str = None):
         """
         vd - Atgriež šodienas vārda dienu jubilārus
         vd [vārds] - Atgriež datumu kurā [vārds] svin vārda dienu
-        vd [datums] - Atgriež [datums] vārda dienas jubilārus (formāts: dd.mm)
+        vd [datums] - Atgriež [datums] vārda dienas jubilārus (formāts: dd.mm; dd/mm; dd,mm)
         """
-        path = os.path.dirname(os.path.realpath(__file__))
-        if os.path.exists('data/namedays/namedays.json'):
-            t = datetime.datetime.now()
-            vd = self.data
-            if msg is None:
+
+        date_regex = re.match("^(0[1-9]|1[0-9]|2[0-9]|3[0-1])(\.|\/|\,)(0[1-9]|1[1-2])$", msg)
+
+        if msg is None:
+            day = datetime.datetime.now().strftime("%d")
+            month = datetime.datetime.now().strftime("%m")
+            result = findByDate(day, month)
+
+            if result:
                 await self.bot.say("%s Šodien vārda dienu svin: `%s`\n\n%s Kalendārā neiekļautie: *`%s`*" % (
-                    emoji, vd["namedays"][int(t.strftime('%j'))-1]["names"].replace(" ", ", "), emoji2,
-                    vd["namedays"][int(t.strftime('%j'))-1]["noncalendarnames"].replace(" ", ", ")))
+                    emoji, result[0].replace(" ", ", "), emoji2, result[1].replace(" ", ", ")))
             else:
-                found = False
-                for day in vd["namedays"]:
-                    date = "%s.%s" % (day["day"], day["month"])
-                    day["date"] = date
-                    if findname(msg)(day["names"]):
-                        await self.bot.say("%s %s vārda dienu svin %s" % (emoji, msg.title(), date))
-                        found = True
-                    if findname(msg)(day["noncalendarnames"]):
-                        await self.bot.say("%s %s vārda dienu svin %s" % (emoji, msg.title(), date))
-                        found = True
-                    if msg == day["date"]:
-                        if day["noncalendarnames"] == "<NAV_NEVIENS_VĀRDS>":
-                            await self.bot.say(
-                                "%s %s vārda dienu svin: `%s`" % (emoji, date, day["names"].replace(" ", ", ")))
-                        else:
-                            await self.bot.say("%s %s vārda dienu svin: `%s`\n\n%s Kalendārā neiekļautie: *`%s`*" % (
-                                emoji, date, day["names"].replace(" ", ", "), emoji2,
-                                day["noncalendarnames"].replace(" ", ", ")))
-                        found = True
-                if not found:
-                    await self.bot.say('Kļūda - "%s" netika atrasts' % (msg))
+                notFound(msg)
+        
+        elif date_regex:
+            day = date_regex.group(1)
+            month = date_regex.group(3)
+            result = findByDate(day, month)
+
+            if result:
+                await self.bot.say("%s Šodien vārda dienu svin: `%s`\n\n%s Kalendārā neiekļautie: *`%s`*" % (
+                    emoji, result[0].replace(" ", ", "), emoji2, result[1].replace(" ", ", ")))
+            else:
+                notFound(msg)
+
         else:
-            await self.bot.say('Something went wrong.')
+            if not findByName(msg):
+                notFound(msg)
+
 
 def check_folder():
     if not os.path.exists('data/namedays'):
